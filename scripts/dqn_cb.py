fed740966914a8935c4ccfbbdd0c89c0c2a54a97
Małgorzata Sudoł <101068576+msudolm@users.noreply.github.com>
2026-04-17 18:45:43 +0200

    chore: change metrics input folder from hardcoded to variable

--- scripts/dqn_cb.py
+++ scripts/dqn_cb.py
@@ -1078,5 +1078,5 @@ if __name__ == "__main__":
     env.plot_results()
     env.stop_simulation()
     clear_SUMO_files(os.path.join(records_folder, "SUMO_output"), os.path.join(records_folder, "episodes"), remove_additional_files=True)
-    run_metrics_analysis(exp_id, results_folder="../results")
+    run_metrics_analysis(exp_id, results_folder=records_folder)
 
